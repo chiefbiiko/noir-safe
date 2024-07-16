74b77da74aa98ee8c503e4be94f7780c22ba547c
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-07-17 00:46:56 +0200

    refactor: make var arrays fixed size in prelude

--- prelude/lib.rs
+++ prelude/lib.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result};
 use ethers::{
     providers::{Middleware, Provider},
-    types::{Address, Block, H256},
+    types::{Address, Block, Bytes, H256},
 };
 use rlp::RlpStream;
 use serde::{Deserialize, Serialize};
@@ -14,15 +14,18 @@ pub const SAFE_SIGNED_MESSAGES_SLOT: [u8; 32] = [
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Inputs {
-    pub safe_address: [u8; 20],     // Safe address
-    pub msg_hash: [u8; 32],         // Custom msg hash
-    pub state_root: [u8; 32],       // eth_getBlockBy*::response.stateRoot
-    pub storage_root: [u8; 32],     // eth_getProof::response.storageHash
-    pub state_trie_key: [u8; 32],   // keccak256(safe)
-    pub storage_trie_key: [u8; 32], // keccak256(msg_hash + uint256(7))
-    pub account_proof: Vec<u8>,     // eth_getProof::response.accountProof
-    pub storage_proof: Vec<u8>,     // eth_getProof::response.storageProof.proof
-    pub header_rlp: Vec<u8>,        // RLP-encoded header
+    pub safe_address: [u64; 20],     // Safe address
+    pub msg_hash: [u64; 32],         // Custom msg hash
+    pub state_root: [u64; 32],       // eth_getBlockBy*::response.stateRoot
+    pub storage_root: [u64; 32],     // eth_getProof::response.storageHash
+    pub state_trie_key: [u64; 32],   // keccak256(safe)
+    pub storage_trie_key: [u64; 32], // keccak256(msg_hash + uint256(7))
+    #[serde(with = "serde_arrays")]
+    pub account_proof: [u64; 4096],  // eth_getProof::response.accountProof
+    #[serde(with = "serde_arrays")]
+    pub storage_proof: [u64; 4096],  // eth_getProof::response.storageProof.proof
+    #[serde(with = "serde_arrays")]
+    pub header_rlp: [u64; 590],      // RLP-encoded header
 }
 
 pub async fn fetch_inputs(
@@ -63,11 +66,38 @@ pub async fn fetch_inputs(
                 .collect(),
         },
     ))
+
+    // Ok((
+    //     latest.as_u64(),
+    //     Inputs {
+    //         safe_address: u8_to_u64_arr(safe_address.into()), //safe_address.into()
+    //         msg_hash: u8_to_u64_arr(msg_hash.into()), //msg_hash.into()
+    //         header_rlp: rlp_encode_header(&block), //.into_iter().map(|b| b as u64).collect(), //rlp_encode_header(&block)
+    //         state_root: u8_to_u64_arr(block.state_root.into()), //block.state_root.into(),
+    //         storage_root: u8_to_u64_arr(proof.storage_hash.into()), //proof.storage_hash.into()
+    //         state_trie_key: u8_to_u64_arr(keccak256(&safe_address)), //keccak256(&safe_address)
+    //         storage_trie_key: u8_to_u64_arr(keccak256(&storage_key)), //keccak256(&storage_key)
+    //         account_proof: fixed_size_proof(&proof.account_proof),
+    //             // proof
+    //             //     .account_proof
+    //             //     .iter()
+    //             //     .map(|b| b.as_bytes().to_vec())
+    //             //     .flatten()
+    //             //     .collect(),
+    //         storage_proof: fixed_size_proof(&proof.storage_proof[0].proof)
+    //             // proof.storage_proof[0]
+    //             //     .proof
+    //             //     .iter()
+    //             //     .map(|b| b.as_bytes().to_vec())
+    //             //     .flatten()
+    //             //     .collect(),
+    //     },
+    // ))
 }
 
 // https://ethereum.stackexchange.com/a/67332
 // https://github.com/ethereum/go-ethereum/blob/14eb8967be7acc54c5dc9a416151ac45c01251b6/core/types/block.go#L65
-pub fn rlp_encode_header(block: &Block<H256>) -> Vec<u8> {
+pub fn rlp_encode_header(block: &Block<H256>) -> [u64; 590] {
     let mut rlp = RlpStream::new();
     rlp.begin_list(20);
     rlp.append(&block.parent_hash);
@@ -94,7 +124,8 @@ pub fn rlp_encode_header(block: &Block<H256>) -> Vec<u8> {
             .parent_beacon_block_root
             .expect("parent_beacon_block_root"),
     ); // cancun
-    rlp.out().freeze().into()
+    let bytes: Vec<u8> = rlp.out().freeze().into();
+    bytes.into_iter().map(|b| b as u64).collect::<Vec<u64>>().try_into().expect("header_rlp")
 }
 
 pub fn concat_bytes64(a: [u8; 32], b: [u8; 32]) -> [u8; 64] {
@@ -108,4 +139,18 @@ pub fn keccak256<T: AsRef<[u8]>>(input: T) -> [u8; 32] {
     k.update(input.as_ref());
     k.finalize(&mut out);
     out
+}
+
+fn u8_to_u64_arr<const L: usize>(byte_array: [u8; L]) -> [u64; L] {
+    byte_array.as_bytes().into_iter().map(|b| *b as u64).collect::<Vec<u64>>().try_into().unwrap()
+}
+
+fn fixed_size_proof(proof: &[Bytes]) -> [u64; 4096] {
+   proof.iter()
+    .map(|b| b.as_bytes().to_vec())
+    .flatten()
+    .map(|b| b as u64) //
+    .collect::<Vec<u64>>()
+    .try_into()
+    .expect("proof")
 }
