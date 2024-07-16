5bfedca2fe0bb78e0bbde5756f753cb541652c0d
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-07-17 00:52:48 +0200

    wip: omni prelude

--- prelude/lib.rs
+++ prelude/lib.rs
@@ -12,6 +12,18 @@ pub const SAFE_SIGNED_MESSAGES_SLOT: [u8; 32] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
 ];
 
+// #[derive(Clone, Debug, Serialize, Deserialize)]
+// pub struct Inputs {
+//     pub safe_address: [u8; 20],     // Safe address
+//     pub msg_hash: [u8; 32],         // Custom msg hash
+//     pub state_root: [u8; 32],       // eth_getBlockBy*::response.stateRoot
+//     pub storage_root: [u8; 32],     // eth_getProof::response.storageHash
+//     pub state_trie_key: [u8; 32],   // keccak256(safe)
+//     pub storage_trie_key: [u8; 32], // keccak256(msg_hash + uint256(7))
+//     pub account_proof: Vec<u8>,     // eth_getProof::response.accountProof
+//     pub storage_proof: Vec<u8>,     // eth_getProof::response.storageProof.proof
+//     pub header_rlp: Vec<u8>,        // RLP-encoded header
+// }
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Inputs {
     pub safe_address: [u64; 20],     // Safe address
@@ -42,59 +54,77 @@ pub async fn fetch_inputs(
         .get_proof(safe_address, vec![storage_key.into()], Some(latest.into()))
         .await?;
 
-    Ok((
-        latest.as_u64(),
-        Inputs {
-            safe_address: safe_address.into(),
-            msg_hash: msg_hash.into(),
-            header_rlp: rlp_encode_header(&block),
-            state_root: block.state_root.into(),
-            storage_root: proof.storage_hash.into(),
-            state_trie_key: keccak256(&safe_address),
-            storage_trie_key: keccak256(&storage_key),
-            account_proof: proof
-                .account_proof
-                .iter()
-                .map(|b| b.as_bytes().to_vec())
-                .flatten()
-                .collect(),
-            storage_proof: proof.storage_proof[0]
-                .proof
-                .iter()
-                .map(|b| b.as_bytes().to_vec())
-                .flatten()
-                .collect(),
-        },
-    ))
-
     // Ok((
     //     latest.as_u64(),
     //     Inputs {
-    //         safe_address: u8_to_u64_arr(safe_address.into()), //safe_address.into()
-    //         msg_hash: u8_to_u64_arr(msg_hash.into()), //msg_hash.into()
-    //         header_rlp: rlp_encode_header(&block), //.into_iter().map(|b| b as u64).collect(), //rlp_encode_header(&block)
-    //         state_root: u8_to_u64_arr(block.state_root.into()), //block.state_root.into(),
-    //         storage_root: u8_to_u64_arr(proof.storage_hash.into()), //proof.storage_hash.into()
-    //         state_trie_key: u8_to_u64_arr(keccak256(&safe_address)), //keccak256(&safe_address)
-    //         storage_trie_key: u8_to_u64_arr(keccak256(&storage_key)), //keccak256(&storage_key)
-    //         account_proof: fixed_size_proof(&proof.account_proof),
-    //             // proof
-    //             //     .account_proof
-    //             //     .iter()
-    //             //     .map(|b| b.as_bytes().to_vec())
-    //             //     .flatten()
-    //             //     .collect(),
-    //         storage_proof: fixed_size_proof(&proof.storage_proof[0].proof)
-    //             // proof.storage_proof[0]
-    //             //     .proof
-    //             //     .iter()
-    //             //     .map(|b| b.as_bytes().to_vec())
-    //             //     .flatten()
-    //             //     .collect(),
+    //         safe_address: safe_address.into(),
+    //         msg_hash: msg_hash.into(),
+    //         header_rlp: rlp_encode_header(&block),
+    //         state_root: block.state_root.into(),
+    //         storage_root: proof.storage_hash.into(),
+    //         state_trie_key: keccak256(&safe_address),
+    //         storage_trie_key: keccak256(&storage_key),
+    //         account_proof: proof
+    //             .account_proof
+    //             .iter()
+    //             .map(|b| b.as_bytes().to_vec())
+    //             .flatten()
+    //             .collect(),
+    //         storage_proof: proof.storage_proof[0]
+    //             .proof
+    //             .iter()
+    //             .map(|b| b.as_bytes().to_vec())
+    //             .flatten()
+    //             .collect(),
     //     },
     // ))
+    Ok((
+        latest.as_u64(),
+        Inputs {
+            safe_address: u8_to_u64_arr(safe_address.into()),
+            msg_hash: u8_to_u64_arr(msg_hash.into()),
+            header_rlp: rlp_encode_header(&block),
+            state_root: u8_to_u64_arr(block.state_root.into()),
+            storage_root: u8_to_u64_arr(proof.storage_hash.into()),
+            state_trie_key: u8_to_u64_arr(keccak256(&safe_address)),
+            storage_trie_key: u8_to_u64_arr(keccak256(&storage_key)),
+            account_proof: fixed_size_proof(&proof.account_proof),
+            storage_proof: fixed_size_proof(&proof.storage_proof[0].proof)
+        },
+    ))
 }
 
+// // https://ethereum.stackexchange.com/a/67332
+// // https://github.com/ethereum/go-ethereum/blob/14eb8967be7acc54c5dc9a416151ac45c01251b6/core/types/block.go#L65
+// pub fn rlp_encode_header(block: &Block<H256>) -> Vec<u8> {
+//     let mut rlp = RlpStream::new();
+//     rlp.begin_list(20);
+//     rlp.append(&block.parent_hash);
+//     rlp.append(&block.uncles_hash);
+//     rlp.append(&block.author.expect("author"));
+//     rlp.append(&block.state_root);
+//     rlp.append(&block.transactions_root);
+//     rlp.append(&block.receipts_root);
+//     rlp.append(&block.logs_bloom.expect("logs_bloom"));
+//     rlp.append(&block.difficulty);
+//     rlp.append(&block.number.expect("number"));
+//     rlp.append(&block.gas_limit);
+//     rlp.append(&block.gas_used);
+//     rlp.append(&block.timestamp);
+//     rlp.append(&block.extra_data.as_bytes().to_vec());
+//     rlp.append(&block.mix_hash.expect("mix_hash"));
+//     rlp.append(&block.nonce.expect("nonce"));
+//     rlp.append(&block.base_fee_per_gas.expect("base_fee_per_gas")); // london
+//     rlp.append(&block.withdrawals_root.expect("withdrawals_root")); // shanghai
+//     rlp.append(&block.blob_gas_used.expect("blob_gas_used")); // cancun
+//     rlp.append(&block.excess_blob_gas.expect("excess_blob_gas")); // cancun
+//     rlp.append(
+//         &block
+//             .parent_beacon_block_root
+//             .expect("parent_beacon_block_root"),
+//     ); // cancun
+//     rlp.out().freeze().into()
+// }
 // https://ethereum.stackexchange.com/a/67332
 // https://github.com/ethereum/go-ethereum/blob/14eb8967be7acc54c5dc9a416151ac45c01251b6/core/types/block.go#L65
 pub fn rlp_encode_header(block: &Block<H256>) -> [u64; 590] {
