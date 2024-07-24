fa2b089af7fcb3fc96b8a38edc1ed05ebf8950b1
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-07-24 15:17:14 +0200

    wip: verify aggregated proof

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -6,5 +6,8 @@ B=~/.nargo/backends/acvm-backend-barretenberg/backend_binary
 D=$(git rev-parse --show-toplevel)
 
 # verify that the generated recursive proof is valid
-$B write_vk -b $D/target/noir_safe_aggregation_circuit.json -o $D/target/ag_vk
+jq -r '.bytecode' $D/target/noir_safe_aggregation_circuit.json | base64 -d > $D/target/noir_safe_aggregation_circuit.gz
+$B write_vk -b $D/target/noir_safe_aggregation_circuit.gz -o $D/target/ag_vk
+#TODO pass proof as bin
+#xxd -r -p $D/proofs/noir_safe_account_proof_circuit.proof $D/proofs/noir_safe_account_proof_circuit.proof.bin
 $B verify -p $D/proofs/noir_safe_aggregation_circuit.proof -k $D/target/ag_vk
