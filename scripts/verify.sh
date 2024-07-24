c77e79acb82cb417dad5970e0bae185b3630164a
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-07-24 14:04:59 +0200

    wip: proof as fields len ?

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -2,8 +2,9 @@
 
 set -ueExo pipefail
 
+b=~/.nargo/backends/acvm-backend-barretenberg/backend_binary
 d=$(git rev-parse --show-toplevel)
 
 # verify that the generated recursive proof is valid
-bb write_vk -b $d/target/noir_safe_aggregation_circuit.json -o $d/target/ag_vk
-bb verify -p $d/proofs/noir_safe_aggregation_circuit.proof -k $d/target/ag_vk
+$b write_vk -b $d/target/noir_safe_aggregation_circuit.json -o $d/target/ag_vk
+$b verify -p $d/proofs/noir_safe_aggregation_circuit.proof -k $d/target/ag_vk
