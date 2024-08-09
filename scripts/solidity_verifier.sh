12045cd24ad04889f65dfc93c76960b4dbe1ed9f
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-08-09 14:21:48 +0200

    scripts: verify.xylfdslfd

--- scripts/solidity_verifier.sh
+++ scripts/solidity_verifier.sh
@@ -5,22 +5,20 @@ set -ueExo pipefail
 B=~/.nargo/backends/acvm-backend-barretenberg/backend_binary
 D=$(git rev-parse --show-toplevel)
 
-# nargo codegen-verifier
-
+cp $D/target/ag_vk $D/target/vk
 $B contract -o $D/contract/UltraVerifier.sol
 
+# PROOF_PATH=$D/target/ag_proof.bin
 
-PROOF_PATH=$D/proofs/noir_safe_aggregation_proof. #./target/proof
-# $BACKEND prove -b ./target/hello_world.json -w ./target/witness.gz -o $PROOF_PATH
-
-NUM_PUBLIC_INPUTS=1
+NUM_PUBLIC_INPUTS=2
 PUBLIC_INPUT_BYTES=$((32 * $NUM_PUBLIC_INPUTS))
-HEX_PUBLIC_INPUTS=$(head -c $PUBLIC_INPUT_BYTES $PROOF_PATH | od -An -v -t x1 | tr -d $' \n')
-HEX_PROOF=$(tail -c +$(($PUBLIC_INPUT_BYTES + 1)) $PROOF_PATH | od -An -v -t x1 | tr -d $' \n')
+# HEX_PUBLIC_INPUTS=$(head -c $PUBLIC_INPUT_BYTES $PROOF_PATH | od -An -v -t x1 | tr -d $' \n')
+blockhash=$(yq -r '.blockhash' $D/circuits/anchor/Prover.toml)
+challenge=$(yq -r '.challenge' $D/circuits/anchor/Prover.toml)
+HEX_PROOF=$(tail -c +$(($PUBLIC_INPUT_BYTES + 1)) $D/target/ag_proof.bin | od -An -v -t x1 | tr -d $' \n')
 
-
-# Spin up an anvil node to deploy the contract to
 anvil &
+anvil_pid=$!
 
 DEPLOY_INFO=$(forge create --contracts $D/contract UltraVerifier \
     --rpc-url "127.0.0.1:8545" \
@@ -28,9 +26,6 @@ DEPLOY_INFO=$(forge create --contracts $D/contract UltraVerifier \
     --json)
 VERIFIER_ADDRESS=$(echo $DEPLOY_INFO | jq -r '.deployedTo')
 
-# Call the verifier contract with our proof.
-# Note that we haven't needed to split up `HEX_PUBLIC_INPUTS` as there's only a single public input
-cast call $VERIFIER_ADDRESS "verify(bytes, bytes32[])(bool)" "0x$HEX_PROOF" "[0x$HEX_PUBLIC_INPUTS]"
+cast call $VERIFIER_ADDRESS "verify(bytes, bytes32[])(bool)" "0x$HEX_PROOF" "[${blockhash#0x}, ${challenge#0x}]"
 
-# Stop anvil node again
-kill %-
+kill $anvil_pid
