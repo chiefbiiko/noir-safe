a8d1513f27d2bcb095182badd41f317ada6eda9c
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-08-12 10:19:41 +0200

    refactor: use front- and backends correctly

--- scripts/aggregate.sh
+++ scripts/aggregate.sh
@@ -9,13 +9,8 @@ B=~/.nargo/backends/acvm-backend-barretenberg/backend_binary
 D=$(git rev-parse --show-toplevel)
 VK_TOML=$D/target/vk.toml
 AGGREGATION_PROVER_TOML=$D/circuits/aggregation/Prover.toml
-# AGGREGATION_VERIFIER_TOML=$D/circuits/aggregation/Verifier.toml
 
 ###
-
-# nargo prove --package noir_safe_storage_proof_circuit
-# xxd -r -p $D/proofs/noir_safe_storage_proof_circuit.proof $D/proofs/noir_safe_storage_proof_circuit.proof.bin
-# SP_FULL_PROOF_AS_FIELDS="$($B proof_as_fields -p $D/proofs/noir_safe_storage_proof_circuit.proof.bin -k $D/target/sp_vk -o -)"
 
 nargo execute --package noir_safe_storage_proof_circuit sp_witness
 $B prove -b $D/target/sp_circuit -w $D/target/sp_witness.gz -o $D/target/sp_proof.bin
@@ -23,29 +18,11 @@ SP_FULL_PROOF_AS_FIELDS="$($B proof_as_fields -p $D/target/sp_proof.bin -k $D/ta
 
 ###
 
-# nargo prove --package noir_safe_account_proof_circuit
-# xxd -r -p $D/proofs/noir_safe_account_proof_circuit.proof $D/proofs/noir_safe_account_proof_circuit.proof.bin
-# AP_FULL_PROOF_AS_FIELDS="$($B proof_as_fields -p $D/proofs/noir_safe_account_proof_circuit.proof.bin -k $D/target/ap_vk -o -)"
-
 nargo execute --package noir_safe_account_proof_circuit ap_witness
 $B prove -b $D/target/ap_circuit -w $D/target/ap_witness.gz -o $D/target/ap_proof.bin
 AP_FULL_PROOF_AS_FIELDS="$($B proof_as_fields -p $D/target/ap_proof.bin -k $D/target/ap_vk -o -)"
 
 ###
-
-# nargo prove --package noir_safe_anchor_circuit
-
-# # If program has any public inputs/return values these need to be prepended
-# #SEE https://discord.com/channels/1113924620781883405/1113924622031798313/1182461084616118363
-# blockhash=$(yq -r '.blockhash' $D/circuits/anchor/Prover.toml)
-# challenge=$(yq -r '.challenge' $D/circuits/anchor/Prover.toml)
-# echo ${blockhash#0x} > $D/proofs/noir_safe_anchor_circuit.proof.out
-# echo ${challenge#0x} >> $D/proofs/noir_safe_anchor_circuit.proof.out
-# cat $D/proofs/noir_safe_anchor_circuit.proof >> $D/proofs/noir_safe_anchor_circuit.proof.out
-
-# xxd -r -p $D/proofs/noir_safe_anchor_circuit.proof.out $D/proofs/noir_safe_anchor_circuit.proof.out.bin
-# AN_FULL_PROOF_AS_FIELDS="$($B proof_as_fields -p $D/proofs/noir_safe_anchor_circuit.proof.out.bin -k $D/target/an_vk -o -)"
-# AN_PROOF_AS_FIELDS=$(echo $AN_FULL_PROOF_AS_FIELDS | jq -r '.[2:]')
 
 nargo execute --package noir_safe_anchor_circuit an_witness
 $B prove -b $D/target/an_circuit -w $D/target/an_witness.gz -o $D/target/an_proof.bin
@@ -64,10 +41,6 @@ ap_pi = []
 an_proof = $AN_PROOF_AS_FIELDS
 an_pi = [\"$blockhash\", \"$challenge\"]
 " >> $AGGREGATION_PROVER_TOML
-
-# echo "an_pi = [\"$blockhash\", \"$challenge\"]" > $AGGREGATION_VERIFIER_TOML
-
-# nargo prove --package noir_safe_aggregation_circuit
 
 nargo execute --package noir_safe_aggregation_circuit ag_witness
 $B prove -b $D/target/ag_circuit -w $D/target/ag_witness.gz -o $D/target/ag_proof.bin
--- scripts/verify.sh
+++ scripts/verify.sh
@@ -5,17 +5,6 @@ set -ueExo pipefail
 B=~/.nargo/backends/acvm-backend-barretenberg/backend_binary
 D=$(git rev-parse --show-toplevel)
 
-# blockhash=$(yq -r '.blockhash' $D/circuits/anchor/Prover.toml)
-# challenge=$(yq -r '.challenge' $D/circuits/anchor/Prover.toml)
-# echo ${blockhash#0x} > $D/proofs/noir_safe_aggregation_circuit.proof.out
-# echo ${challenge#0x} >> $D/proofs/noir_safe_aggregation_circuit.proof.out
-# cat $D/proofs/noir_safe_aggregation_circuit.proof >> $D/proofs/noir_safe_aggregation_circuit.proof.out
-# xxd -r -p $D/proofs/noir_safe_aggregation_circuit.proof.out $D/proofs/noir_safe_aggregation_circuit.proof.out.bin
-# $B verify -p $D/proofs/noir_safe_aggregation_circuit.proof.out.bin -k $D/target/ag_vk
-
-# xxd -r -p $D/proofs/noir_safe_aggregation_circuit.proof $D/proofs/noir_safe_aggregation_circuit.proof.bin
-# $B verify -p $D/proofs/noir_safe_aggregation_circuit.proof.bin -k $D/target/ag_vk
-
 #NOTE
 # wen declaring the aggregation circuit's an_pi input as public the proof has length 
 # 2144 (proof) + 512 (accumulator) + 64 (an_pi)
