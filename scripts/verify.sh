9514c81b49b48b45265b70dcf7c4f334801ed3ae
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-08-26 11:51:33 +0200

    arte: verifiaction key hash mismatch between .sol and vk_hash

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -12,7 +12,7 @@ hex_proof=$(tail -c +$(($pub_bytes + 1)) $d/target/ag_proof.bin | od -An -v -t x
 anvil &
 anvil_pid=$!
 deploy_info=$( \
-    forge create --no-cache --force --contracts $d/contract UltraVerifier \
+    forge create --no-cache --force --contracts $d UltraVerifier \
         --rpc-url "127.0.0.1:8545" \
         --private-key "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" \
         --json \
