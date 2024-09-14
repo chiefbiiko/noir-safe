362e6b55188451810d8a6c2aff280d7914fd5228
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-09-14 12:43:44 +0200

    refactor: leftover

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,8 +6,8 @@ d=$(git rev-parse --show-toplevel)
 
 $d/scripts/compile.sh
 
-cargo run --manifest-path $d/prelude/Cargo.toml
+REQ_ID=123 cargo run --manifest-path $d/prelude/Cargo.toml
 
-$d/scripts/aggregate.sh
+REQ_ID=123 $d/scripts/aggregate.sh
 
 $d/scripts/verify.sh
